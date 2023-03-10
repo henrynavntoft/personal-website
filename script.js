95872e45e2ba8a1033baf8b04dcef5d70dafeae4
Henry Navntoft <henrylnavntoft@gmail.com>
2023-03-10 09:30:07 +0100

    First Draft

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+/*PRELOADER*/
+
+let loader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    loader.classList.add("preloader-fade");
+  }, 1200);
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 1500);
+});
