fbd866e9d4839ddb772bd5b1ce688e60b4053771
Henry Navntoft <henrylnavntoft@gmail.com>
2023-03-16 13:26:45 +0100

    Boom

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 /*PRELOADER*/
 
@@ -11,4 +11,29 @@ window.addEventListener("load", function () {
   setTimeout(function () {
     loader.style.display = "none";
   }, 1500);
+});
+
+let theme = localStorage.getItem("data-theme");
+const checkbox = document.getElementById("switch");
+const changeThemeToDark = () => {
+  document.documentElement.setAttribute("data-theme", "dark");
+  localStorage.setItem("data-theme", "dark");
+};
+
+const changeThemeToLight = () => {
+  document.documentElement.setAttribute("data-theme", "light");
+  localStorage.setItem("data-theme", "light");
+};
+
+if (theme === "dark") {
+  changeThemeToDark();
+}
+
+checkbox.addEventListener("change", () => {
+  let theme = localStorage.getItem("data-theme");
+  if (theme === "dark") {
+    changeThemeToLight();
+  } else {
+    changeThemeToDark();
+  }
 });
