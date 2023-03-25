d899f697425ad781be31b85060b5160f0877386c
Henry Navntoft <henrylnavntoft@gmail.com>
2023-03-25 12:07:30 +0100

    PROGRESSING

--- README.md
+++
@@ -1,5 +0,0 @@
-1. Add a few projects, hosted from github
-1. Dark/light theme?
-1. Make a nice simple footer
-
-..... Need to add some kind of JS
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//"use strict";
+"use strict";
 
-/*PRELOADER*/
+//PRELOADER
 
 let loader = document.querySelector(".preloader");
 
@@ -13,27 +13,59 @@ window.addEventListener("load", function () {
   }, 1500);
 });
 
-let theme = localStorage.getItem("data-theme");
-const checkbox = document.getElementById("switch");
-const changeThemeToDark = () => {
-  document.documentElement.setAttribute("data-theme", "dark");
-  localStorage.setItem("data-theme", "dark");
-};
+//THEME SWITCHER
 
-const changeThemeToLight = () => {
-  document.documentElement.setAttribute("data-theme", "light");
-  localStorage.setItem("data-theme", "light");
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.getElementById("theme-toggle");
 
-if (theme === "dark") {
-  changeThemeToDark();
-}
+  const htmlElement = document.documentElement;
 
-checkbox.addEventListener("change", () => {
-  let theme = localStorage.getItem("data-theme");
-  if (theme === "dark") {
-    changeThemeToLight();
-  } else {
-    changeThemeToDark();
-  }
+  themeToggle.addEventListener("change", () => {
+    if (htmlElement.getAttribute("data-theme") === "light") {
+      htmlElement.setAttribute("data-theme", "dark");
+    } else {
+      htmlElement.setAttribute("data-theme", "light");
+    }
+  });
 });
+
+//BURGERMENU
+
+document.querySelector(".burger-menu").addEventListener("click", function () {
+  let dropdownMenu = document.querySelector(".dropdown-menu");
+  let burger = document.querySelector(".burger");
+
+  if (dropdownMenu.style.display === "flex") {
+    dropdownMenu.style.display = "none";
+    burger.classList.remove("burger-open");
+  } else {
+    dropdownMenu.style.display = "flex";
+    burger.classList.add("burger-open");
+    let items = document.querySelectorAll(".dropdown-item");
+    items.forEach(function (item, index) {
+      item.style.animation = "none";
+      item.offsetHeight;
+      item.style.animation = "dropdown-item-fall 0.5s forwards";
+      item.style.animationDelay = index * 0.1 + "s";
+    });
+  }
+  let items = document.querySelectorAll(".dropdown-item");
+
+  items.forEach(function (item) {
+    item.addEventListener("click", function () {
+      dropdownMenu.style.display = "none";
+      burger.classList.remove("burger-open");
+    });
+  });
+});
+
+console.log(`
+ _   _ 
+| | | |
+| |_| |
+|  _  |
+| | | |
+|_| |_|
+
+
+`);
